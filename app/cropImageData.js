3015c2432920b46ca7968b8ddbb58b14fdd4a9b3
Patrick Sinclair <metade@gmail.com>
2014-01-17 10:58:19 +0000

    Made crop region with angle work

--- app/cropImageData.js
+++ app/cropImageData.js
@@ -1,25 +1,39 @@
 define("cropImageData", [], function() {
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  document.body.insertBefore(canvas, document.body.childNodes[0]);
-
-
   return function cropImageData(image, region) {
-    canvas.width = image.width;
-    canvas.height = image.width;
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+
     ctx.clearRect(0, 0, image.width, image.height);
     ctx.putImageData(image, 0, 0);
 
     if (region.angle) {
+      ctx.clearRect(0, 0, image.width, image.height);
+      ctx.putImageData(image, 0, 0);
+
+      max = Math.max(region.width, region.height)
+      diameter = Math.floor(max * 1.5);
+      radius = Math.floor(diameter/2);
+      area = ctx.getImageData(region.x-radius, region.y-radius, diameter, diameter);
+
+      ctx.strokeStyle = "#00FF00";
+      ctx.strokeRect(region.x-radius, region.y-radius, diameter, diameter);
+
+      ctx.clearRect(0, 0, diameter, diameter);
+      canvas.width=diameter;
+      canvas.height=diameter;
+
+      ctx.putImageData(area, 0, 0);
+
       ctx.save();
-      ctx.translate(region.x, region.y)
+      ctx.translate(radius, radius);
       ctx.rotate(-region.angle);
-      ctx.drawImage(canvas, -image.width/2, -image.height/2);
-      ctx.rotate(region.angle);
+      ctx.drawImage(canvas, -radius, -radius);
       ctx.restore();
 
-      return ctx.getImageData(region.x - region.width/2, region.y - region.width/2, region.width, region.height);
+      var x = Math.floor(radius-region.width/2), y = Math.floor(radius-region.height/2);
+      return ctx.getImageData(x, y, region.width, region.height);
     } else {
+
       var x = Math.floor(region.x - (region.width/2)),
           y = Math.floor(region.y - region.height/2);
       return ctx.getImageData(x, y, region.height, region.width);
--- spec/cropImageDataSpec.js
+++ spec/cropImageDataSpec.js
@@ -9,8 +9,8 @@ define(['cropImageData'],function(cropImageData) {
     });
 
     afterEach(function() {
-      // ctx.clearRect(0, 0, 10, 10);
-      // ctx.putImageData(result, 0, 0);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.putImageData(result, 0, 0);
     });
 
     it("should crop a 3x3 region", function() {
@@ -30,27 +30,65 @@ define(['cropImageData'],function(cropImageData) {
       }
     });
 
-    it("should crop a 3x3 region at an angle", function() {
+    function drawRegion(region) {
       ctx.fillStyle = '#FFFFFF';
       ctx.fillRect(0, 0, 100, 100);
-
-      region = {
-        x: 50, y: 50, width: 50, height: 50, angle: Math.PI * Math.random()
-      };
 
       ctx.save();
       ctx.translate(region.x, region.y)
       ctx.rotate(region.angle);
       ctx.fillStyle = '#000000';
-      ctx.fillRect(-region.x/2, -region.y/2, region.height, region.width);
-      ctx.restore();
+      ctx.fillRect(-region.width/2, -region.height/2, region.width, region.height);
 
-      target = ctx.getImageData(0, 0, 100, 100);
+      // debug
+      ctx.fillStyle = '#CCCCCC';
+      ctx.fillRect(-1, -1, 3, 3);
+
+      ctx.restore();
+      return ctx.getImageData(0, 0, 100, 100);
+    }
+
+    it("should crop a 50x50 region", function() {
+      region = {
+        x: 50,
+        y: 50,
+        width: 50,
+        height: 50,
+        angle: Math.PI * Math.random()
+      };
+      target = drawRegion(region);
       result = cropImageData(target, region);
-      ctx.putImageData(result, 120, 20);
 
       expect(result.width).toEqual(50);
       expect(result.height).toEqual(50);
+      for (var i=0; i<result.data.length; i+=4) {
+        if (result.data[i]==0) {
+          expect(result.data[i]).toEqual(0);
+          expect(result.data[i+1]).toEqual(0);
+          expect(result.data[i+2]).toEqual(0);
+        } else {
+          // should be gray and not white
+          var r = result.data[i];
+          expect(r).toNotEqual(255);
+          expect(result.data[i+1]).toEqual(r);
+          expect(result.data[i+2]).toEqual(r);
+        }
+      }
+    });
+
+    it("should crop a 50x50 region at an angle", function() {
+      region = {
+        x: 25,
+        y: 25,
+        width: 10,
+        height: 25,
+        angle: Math.PI * Math.random()
+      };
+      target = drawRegion(region);
+      result = cropImageData(target, region);
+
+      expect(result.width).toEqual(region.width);
+      expect(result.height).toEqual(region.height);
       for (var i=0; i<result.data.length; i+=4) {
         if (result.data[i]==0) {
           expect(result.data[i]).toEqual(0);
